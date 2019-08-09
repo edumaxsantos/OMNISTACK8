089fef31ccbc67d158b8026f9ce3306c57add4a5
Eduardo M. Cardoso Santos <eduardo.cardoso@linkconsulting.com.br>
2019-08-09 14:34:45 -0300

    Adicionado websocket

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,6 +2,8 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res) {
+
+        console.log(req.io, req.connectedUsers);
         
         const { user } = req.headers;
         const { devId } = req.params;
@@ -16,7 +18,15 @@ module.exports = {
         console.log(`${loggedDev.name} deu like em ${targetDev.name}`);
 
         if(targetDev.likes.includes(loggedDev._id)) {
-            console.log('DEU MATCH');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if(loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+            if(targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
